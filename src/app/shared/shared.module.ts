6b73a67cb28c2d7df1f0abf6fb9eae2d8b1ce6ab
ChristianCJD <Christiancjd90@gmail.com>
2024-10-10 09:52:20 -0400

    pages and routing setup

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,21 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomePageComponent } from './pages/home-page/home-page.component';
+import { RouterModule } from '@angular/router';
+
 import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 
 
 @NgModule({
   declarations: [
     AboutPageComponent,
-    HomePageComponent
+    ContactComponent,
+    HomePageComponent,
+    SidebarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     AboutPageComponent,
-    HomePageComponent
+    ContactComponent,
+    HomePageComponent,
+    SidebarComponent
   ]
 })
 export class SharedModule { }
